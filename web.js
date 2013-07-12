bda9263aed7c18980251ae984a24d35646d18a61
anchavesb <anchavesb@hotmail.com>
2013-07-11 22:22:59 -0400

    se arregla error

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
 fs.readFileSync('index.html', function (err, data){
-       if (err) throw err;
+       if (err) response.send(err.toString());
        response.send(data.toString());
     }
 });
